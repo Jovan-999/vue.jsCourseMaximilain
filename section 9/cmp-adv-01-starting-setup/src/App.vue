6dfa9f678ae08a28c5c40d17fdc47c922d1aa024
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-05-29 17:36:04 +0200

    vue.js_course

--- section 9/cmp-adv-01-starting-setup/src/App.vue
+++ section 9/cmp-adv-01-starting-setup/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- <TheHeader /> -->
     <the-header></the-header>
     <badge-list></badge-list>
     <user-info
@@ -11,7 +12,16 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
