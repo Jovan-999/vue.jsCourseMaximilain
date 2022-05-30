f7e699be9dfecaff42e53c42fa4c40030cd9a4a3
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-05-30 16:10:32 +0200

    vue.js_course

---
+++ section 10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resources in storedResources"
+      :key="resources.id"
+      :title="resources.title"
+      :description="resources.description"
+      :link="resources.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
