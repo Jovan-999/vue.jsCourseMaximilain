06f37dbb53add075f700b5cb22a9c36182f9a130
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-05-31 16:15:29 +0200

    vue.js_course

--- section 12/http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ section 12/http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -58,7 +58,7 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
+  // emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -67,10 +67,22 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
+
+      fetch(
+        'https://vue-https-demo-40ea1-default-rtdb.europe-west1.firebasedatabase.app/surveys.json',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            name: this.enteredName,
+            rating: this.chosenRating,
+          }),
+        }
+      );
 
       this.enteredName = '';
       this.chosenRating = null;
